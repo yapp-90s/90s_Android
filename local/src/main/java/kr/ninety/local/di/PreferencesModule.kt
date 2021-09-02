1e7fe7d1b9b4423e15f5c2430f09319f79759ade
peterchoee <peter.choee@gmail.com>
2021-09-02 22:15:07 +0900

    SharedPreferences DI Annotation 제거

--- local/src/main/java/kr/ninety/local/di/PreferencesModule.kt
+++ local/src/main/java/kr/ninety/local/di/PreferencesModule.kt
@@ -1,7 +1,6 @@
 package kr.ninety.local.di
 
 import android.content.Context
-import android.content.SharedPreferences
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -9,7 +8,6 @@ import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import kr.ninety.local.preferences.UserPreferences
 import kr.ninety.local.preferences.UserPreferencesImpl
-import javax.inject.Named
 import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
@@ -19,15 +17,6 @@ object PreferencesModule {
     @Provides
     @Singleton
     fun provideUserPreferences(
-        @Named("userSharedPreferences") preferences: SharedPreferences
-    ): UserPreferences = UserPreferencesImpl(preferences)
-
-    @Provides
-    @Singleton
-    @Named("userSharedPreferences")
-    fun provideSharedPreferences(
         @ApplicationContext context: Context
-    ): SharedPreferences = context.getSharedPreferences(
-        "USER", Context.MODE_PRIVATE
-    )
+    ): UserPreferences = UserPreferencesImpl(context)
 }
--- local/src/main/java/kr/ninety/local/preferences/UserPreferencesImpl.kt
+++ local/src/main/java/kr/ninety/local/preferences/UserPreferencesImpl.kt
@@ -1,17 +1,22 @@
 package kr.ninety.local.preferences
 
-import android.content.SharedPreferences
+import android.content.Context
 import javax.inject.Inject
 
 class UserPreferencesImpl @Inject constructor(
-    private val privatePreferences: SharedPreferences
+    private val context: Context
 ) : UserPreferences {
 
+    private val preferences by lazy {
+        context.getSharedPreferences(PREFERENCE_NAME, Context.MODE_PRIVATE)
+    }
+
     override fun getToken(): String {
-        return privatePreferences.getString(KEY_TOKEN, "")!!
+        return preferences.getString(KEY_TOKEN, "")!!
     }
 
     companion object {
+        private const val PREFERENCE_NAME = "USER"
         private const val KEY_TOKEN = "TOKEN"
     }
 }
